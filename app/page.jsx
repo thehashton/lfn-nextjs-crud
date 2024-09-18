b86f60a5f02f4e964cd53ea1dc65e018b1128a17
thehashton <harry.ashton.uk@gmail.com>
2024-09-18 11:39:35 +0100

    Makes it compitable to deploy to vercel

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,16 @@ import styles from "./page.module.css";
 import TaskList from '@/components/TaskList';
 
 export default async function Home() {
-  const response = await fetch('http://localhost:3000/api/tasks', {
-    cache: 'no-store'
-  });
-  const tasks = await response.json();
-  
-  return (
-    <div className={styles.main}>
-      <TaskList />
-    </div>
-  );
+    // Amended this to be able to deploy to Vercel
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'; // Fallback for local development
+    const response = await fetch(`${apiUrl}/api/tasks`, {
+        cache: 'no-store'
+    });
+    const tasks = await response.json();
+
+    return (
+        <div className={styles.main}>
+            <TaskList tasks={tasks} />
+        </div>
+    );
 }
